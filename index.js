6fd923f164800d54705be47ef3c394137593d2dc
Mat'Dev <45517025+MatDev4@users.noreply.github.com>
2019-01-26 15:30:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-/* 
-Soon 
-*/ 
+const Discord = require("discord.js")
+const client = new Discord.Client({disableEveryone: true})
+const Config = require("./config.json")
+client.login(Config.token)
+client.on("ready" () => {
+    client.user.setActivity("ICI LE JEU DU BOT")
+    client.user.setStatus("dnd ou idle ou online")
+})
+client.on("message", message => {
+    if(message.content.startsWith("LE PREFIX DE VOTRE BOT"+ "LE NOM DE VOTRE COMMANDE")) {
+        ICI LE CODE A EXECUTER LORS QU ON EXECUTE CETTE COMMANDE
+    }
+})
